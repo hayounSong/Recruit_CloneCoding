22fb17179e72e0b085be89bc8f7894025e48bd19
hayoun <fishingest@kookmin.ac.kr>
2023-02-20 02:09:46 +0900

    [feat] 파트 초기 데이터 세팅

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into part(part_id,part_name) values(1,'서버');
+insert into part(part_id,part_name) values(2,'웹');
+insert into part(part_id,part_name) values(3,'안드로이드');
+insert into part(part_id,part_name) values(4,'ios');
+insert into part(part_id,part_name) values(5,'디자인');
+insert into part(part_id,part_name) values(6,'기획');
