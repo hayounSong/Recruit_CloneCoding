748228685d7d6e11d8f912c4ea926d88825bc5ee
hayoun <fishingest@kookmin.ac.kr>
2023-02-20 02:16:37 +0900

    [feat] ApplicantRepository 개발

---
+++ src/main/java/com/recruit/recruit_CloneCoding/repository/ApplicantRepository.java
@@ -0,0 +1,35 @@
+package com.recruit.recruit_CloneCoding.repository;
+
+import com.recruit.recruit_CloneCoding.domain.Applicant;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public class ApplicantRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public void save(Applicant applicant){
+        em.persist(applicant);
+    }
+
+    public Applicant findOne(int id){
+        return em.find(Applicant.class,id);
+    }
+
+    public List<Applicant> findAll(){
+        return em.createQuery("select a from Applicant a",Applicant.class).getResultList();
+    }
+
+    public List<Applicant> findByName(String name){
+        return em.createQuery("select a from Applicant a where a.name=:name",Applicant.class).setParameter("name",name).getResultList();
+    }
+
+
+}
