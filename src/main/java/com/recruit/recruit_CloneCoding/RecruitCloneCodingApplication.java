c342e16df130fb399f67825ae12384de87dd22ef
hayoun <fishingest@kookmin.ac.kr>
2023-02-19 22:55:08 +0900

    [feat] 스프링 프로젝트 세팅

---
+++ src/main/java/com/recruit/recruit_CloneCoding/RecruitCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.recruit.recruit_CloneCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitCloneCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruit/recruit_CloneCoding/RecruitCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruit.recruit_CloneCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitCloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
