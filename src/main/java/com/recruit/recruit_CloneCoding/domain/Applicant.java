68980d3a2cf440a507805b9e540c238b5874f7e7
hayoun <fishingest@kookmin.ac.kr>
2023-02-20 02:01:48 +0900

    [feat]Applicat 도메인 모델 패턴 구현

--- src/main/java/com/recruit/recruit_CloneCoding/domain/Applicant.java
+++ src/main/java/com/recruit/recruit_CloneCoding/domain/Applicant.java
@@ -1,13 +1,18 @@
 package com.recruit.recruit_CloneCoding.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.w3c.dom.Text;
 
 import java.awt.*;
 
-@Getter
+@Getter @Setter
 @Entity
+@Table(name="applicants")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Applicant {
 
     @Id
@@ -33,5 +38,19 @@ public class Applicant {
     private AbsenseStatus absenseStatus;
 
 
+    public static Applicant createApplicant(Part part, String name, String address,String subway, String email,String school, String major, boolean isOb, TextArea question1, TextArea question2, TextArea question3){
+        Applicant applicant=new Applicant();
+        applicant.setPart(part);
+        applicant.setName(name);
+        applicant.setSubway(subway);
+        applicant.setSchool(school);
+        applicant.setMajor(major);
+        applicant.setOb(isOb);
+        applicant.setQuestion1(question1);
+        applicant.setQuestion2(question2);
+        applicant.setQuestion3(question3);
+
+        return applicant;
+    }
 
 }
